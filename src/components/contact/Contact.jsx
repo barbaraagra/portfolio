7b0cc3431b58f4fc3f61da851031b417b20ab662
barbaraagra <babs.agra@gmail.com>
2023-06-02 16:03:17 +0100

    funções completadas, falta estilo

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
-import './contact.css'
+import './contact.css';
+import { FiMail } from 'react-icons/fi';
+import { BsPhone } from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_0dit30e', 'template_x9eh9rb', form.current, 'glOL9755I5za3DuO6')
+
+            .then(function (response) {
+                console.log('SUCCESS! THE EMAIL WAS SENT!', response.status, response.text);
+            }, function (error) {
+                console.log('FAILED TO SEND. TRY AGAIN!', error);
+            });
+
+        e.target.reset()
+
+    };
+
     return (
-        <section id='contact'>Contact</section>
+        <section id='contact'>
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className='contact__option'>
+                        <FiMail className='contact__option-icon' />
+                        <h4>Email</h4>
+                        <h5>babs.agra@gmail.com</h5>
+                        <a href="mailto:babs.agra@gmail.com">Send an email</a>
+                    </article>
+
+                    <article className='contact__option'>
+                        <BsPhone className='contact__option-icon' />
+                        <h4>Phone</h4>
+                        <h5>+351 912592474</h5>
+                        <a href="https://wa.me/+351912592474" target='_blank' rel='noreferrer'>Send a message</a>
+                    </article>
+                </div>
+                {/*  END OF CONTACT */}
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name='name' placeholder='Your Full Name' required />
+                    <input type="email" name='email' placeholder='Your Email' required />
+                    <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,93 @@
 import React from 'react';
 import './portfolio.css';
+import img1 from '../../assets/portfolio1.png';
+import img2 from '../../assets/portfolio2.png';
+import img3 from '../../assets/portfolio3.png';
+import img4 from '../../assets/portfolio4.png';
+import img5 from '../../assets/portfolio5.png';
+import img6 from '../../assets/portfolio6.png';
+
 
 const Portfolio = () => {
     return (
-        <section id='portfolio'>Portfolio</section>
+        <section id='portfolio'>
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio__container">
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={img1} alt="game" />
+                    </div>
+                    <h3>DINO ERA</h3>
+                    <h4>HTML | CSS | JavaScript</h4>
+                    <div className="portfolio__item-cta">
+                        <a href="http://github.com/barbaraagra" className='btn'>Github</a>
+                        <a href="https://www.behance.net/barbaraagra1" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+                    </div>
+                </article>
+
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={img2} alt="game" />
+                    </div>
+                    <h3>WANNAGO</h3>
+                    <h4>JavaScript | MongoDB | NodeJS | Bootstrap | CSS</h4>
+                    <div className="portfolio__item-cta">
+                        <a href="http://github.com/barbaraagra" className='btn'>Github</a>
+                        <a href="https://www.behance.net/barbaraagra1" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+                    </div>
+                </article>
+
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={img3} alt="game" />
+                    </div>
+                    <h3>NOMADIC</h3>
+                    <h4>React | CSS | Axios</h4>
+                    <div className="portfolio__item-cta">
+                        <a href="http://github.com/barbaraagra" className='btn'>Github</a>
+                        <a href="https://www.behance.net/barbaraagra1" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+                    </div>
+                </article>
+
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={img4} alt="game" />
+                    </div>
+                    <h3>PRADO DECOR</h3>
+                    <h4>WordPress</h4>
+                    <div className="portfolio__item-cta">
+                        <a href="http://github.com/barbaraagra" className='btn'>Github</a>
+                        <a href="https://www.behance.net/barbaraagra1" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+                    </div>
+                </article>
+
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={img5} alt="game" />
+                    </div>
+                    <h3>OHALO STORE</h3>
+                    <h4>TypeScript</h4>
+                    <div className="portfolio__item-cta">
+                        <a href="http://github.com/barbaraagra" className='btn'>Github</a>
+                        <a href="https://www.behance.net/barbaraagra1" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+                    </div>
+                </article>
+
+                <article className='portfolio__item'>
+                    <div className="portfolio__item-image">
+                        <img src={img6} alt="game" />
+                    </div>
+                    <h3>UDRIVE</h3>
+                    <h4>Figma | Photoshop | Illustrator</h4>
+                    <div className="portfolio__item-cta">
+                        <a href="http://github.com/barbaraagra" className='btn'>Github</a>
+                        <a href="https://www.behance.net/barbaraagra1" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+                    </div>
+                </article>
+            </div>
+        </section>
     )
 }
 
